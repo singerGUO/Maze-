173e1c4f32de9301ae8d1954cf1fc8ec9152cc3f
singerGuo <g294525819@outlook.com>
2019-03-15 00:55:02 -0700

    Finish1

--- src/main/java/datastructures/concrete/ArrayDisjointSet.java
+++ src/main/java/datastructures/concrete/ArrayDisjointSet.java
@@ -12,17 +12,18 @@ public class ArrayDisjointSet<T> implements IDisjointSet<T> {
     // Note: do NOT rename or delete this field. We will be inspecting it
     // directly within our private tests.
     private int[] pointers;
-    private int size,overallSize;
-    private IDictionary<T,Integer> map;
+    private int size;
+    private int overallSize;
+    private IDictionary<T, Integer> map;
     T[] myGenericArray;
 
     // You will probably need to add one or two more fields in order to
     // successfully implement this class.
 
     public ArrayDisjointSet() {
-        // TODO: your code here
+
         overallSize=10;
-        myGenericArray=(T[])new Object[overallSize];
+        myGenericArray = (T[]) new Object[overallSize];
         pointers= new int[overallSize];
         map=new ChainedHashDictionary<>();
 
@@ -35,7 +36,7 @@ public class ArrayDisjointSet<T> implements IDisjointSet<T> {
         if (map.containsKey(item)) {
             throw new IllegalArgumentException();
         }
-        if(size==overallSize){
+        if (size == overallSize) {
             extendsCapacity();
         }
 
@@ -47,8 +48,8 @@ public class ArrayDisjointSet<T> implements IDisjointSet<T> {
 
     public void extendsCapacity(){
         int[]temp=new int[overallSize*2];
-        T[] tempT=(T[])new Object[overallSize*2];
-        for(int i=0;i<size;i++){
+        T[] tempT = (T[]) new Object[overallSize * 2];
+        for (int i = 0; i < size; i++) {
             temp[i]=pointers[i];
             tempT[i]=myGenericArray[i];
         }
@@ -64,18 +65,19 @@ public class ArrayDisjointSet<T> implements IDisjointSet<T> {
         }
 
         int modify = map.get(item);
-        int result = findValue(item,modify);
+        int result = findValue(item, modify);
 
         resetRank(modify, result);
         return result;
 
     }
-        private int findValue(T item,int root){
-            if(pointers[root]<0){
+
+    private int findValue(T item, int root) {
+        if (pointers[root] < 0) {
                 return root;
-            }else{
+        } else {
                 root=pointers[root];
-                return findValue(myGenericArray[root],root);
+            return findValue(myGenericArray[root], root);
             }
         }
 
@@ -92,10 +94,10 @@ public class ArrayDisjointSet<T> implements IDisjointSet<T> {
         int index2=findSet(item2);
         int rankV1=pointers[index1];
         int rankV2=pointers[index2];
-        if(rankV1<=rankV2){
+        if (rankV1 <= rankV2) {
             pointers[index2]=index1;
             pointers[index1]-=1;
-        }else{
+        } else {
 
             pointers[index1]=index2;
             pointers[index2]-=1;
